725aa6fe5af8eacb647ff28bd01bc9cf779b766c
coderpirate-minhajulislam <coderpirate.minhajulislam@gmail.com>
2024-12-05 00:39:50 +0600

    converson of data type

---
+++ 02_javascrpit.js
@@ -0,0 +1,27 @@
+"use strict" // treat all js code as newer version
+
+// alert( 3 + 3);  // we are use in node
+
+//?comment 
+//TODO:comment 
+//!comment 
+//*comment 
+
+//! tc39 or MDN 
+
+let name = "Minhax"
+let age = 13
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// String 
+// Boolean
+// null => standlone value
+// undefined => not value assign
+// Symbol
+
+
+// Object
+console.log(typeof isLoggedIn);
+
---
+++ 03_javascript.js
@@ -0,0 +1,27 @@
+// let score = "34aa"
+
+// console.log(typeof(score));
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33aaa" => NaN
+// true => 1, false => 0
+
+
+// let isLoggedIn = 1
+// let boolenLoggedIn = Boolean(isLoggedIn)
+
+// console.log(boolenLoggedIn);
+
+//  1 => true 0 => false
+// "" => false
+// "Minhaz " => true
+
+
+let someNumberr = 33
+let stringNumber = String(someNumberr)
+console.log(someNumberr);
+console.log(typeof stringNumber);
