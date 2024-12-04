199d6ddbcee1409e1acb48696c3ea63ea5837dc6
coderpirate-minhajulislam <coderpirate.minhajulislam@gmail.com>
2024-12-04 23:56:46 +0600

    javascrpit practice project

---
+++ 01_javascrpt.js
@@ -0,0 +1,13 @@
+const accountId = 14234
+let accountEmail = "admin@gamil.com"
+var accontPassword = "12345"
+accountCity = "Dhaka"
+
+//! without use varable can use memory in javascript but not good for it 
+
+/*
+Not use var because it's issue in scope
+*/
+
+console.log(accountId);
+console.table([accountEmail, accontPassword, accountCity]);
