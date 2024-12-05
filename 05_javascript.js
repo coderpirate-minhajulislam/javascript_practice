170bdc14ef118ba72395c826275299c3e275b6f5
coderpirate-minhajulislam <coderpirate.minhajulislam@gmail.com>
2024-12-05 19:37:26 +0600

    Arry Test

---
+++ 05_javascript.js
@@ -0,0 +1,7 @@
+const myArry = [ 1,2,3,4,5,6,]
+const myArry1 = [ 1,2,3,4,5,6,]
+const myArry3 = myArry + myArry1;
+const new_arry_2 = [...myArry, ...myArry1]
+
+console.log(myArry);
+console.log(typeof(myArry3));
